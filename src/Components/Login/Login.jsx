5d5ef067e944f70fe1d5a248dd7019b407bbf7e0
Raghavendran19 <kpraghavendran@gmail.com>
2025-02-08 09:03:26 +0530

    app

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+
 import './Login.css'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react'
 
 const Login = () => {
+    const data=useRef()
+    const password=useRef()
+    const onChange = () => {
+        localStorage.setItem('username',data.current.value)
+        localStorage.setItem('password',password.current.value)
+    }
+    
+
     return (
 
-        <form className='login' action="">
+        <form className='login'>
             <div className="title">
                 <div className="login2">Login</div>
             </div>
             <div className="credentials">
-                <input className='username' placeholder='Username' type="username" />
-                <input className='password' placeholder='Password' type="password" />
+                <input className='username' placeholder='Username' type="username" ref={data} />
+                <input className='password' placeholder='Password' type="password" ref={password}/>
             </div>
             <a href='#' className="msg">Forgot Password</a>
             <Link to='/signup' className='signup msg'>New User</Link>
-            <button type="submit" className='submit'>Submit</button>
+            <button type="submit" className='submit' onClick={onChange()}>Submit</button>
         </form>
 
 
--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import { useRef } from 'react'
 import './Signup.css'
 import { Link } from 'react-router-dom'
 
 const Signup = () => {
+    const name=useRef()
+    const organization=useRef()
+    const email=useRef()
+    const password=useRef()
+    const re_password=useRef()
+    const onStore=()=>{
+        localStorage.setItem('Name',name.current.value)
+        localStorage.setItem('organization',organization.current.value)
+        localStorage.setItem('email',email.current.value)
+        localStorage.setItem('password',password.current.value)
+        localStorage.setItem('re_password',re_password.current.value)
+    }
     return (
 
-        <form className='Signup' action="">
+        <form className='Signup' >
             <div className="title">
                 <div className="Signup2">Signup</div>
             </div>
             <div className="credentials">
-                <input className='name' placeholder='Name' type="name" />
-                <input className='organization' placeholder='Organization' type="text" />
-                <input className='email' placeholder='Email' type="email" />
-                <input className='password' placeholder='Password' type="password" />
-                <input className='re-password password' placeholder='Retype Password' type="password" />
+                <input className='name' placeholder='Name' type="name" ref={name} />
+                <input className='organization' placeholder='Organization' type="text" ref={organization}/>
+                <input className='email' placeholder='Email' type="email" ref={email} />
+                <input className='password' placeholder='Password' type="password" ref={password}/>
+                <input className='re-password password' placeholder='Retype Password' type="password" ref={re_password} />
             </div>
             <Link to='/login' className='signup msg'>Existing User</Link>
-            <button type="submit" className='submit'>Submit</button>
+            <button type="submit" className='submit' onClick={onStore()}>Submit</button>
         </form>
 
 
