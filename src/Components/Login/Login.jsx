9001304cadf988c6c76d7a447f3872145dad41ce
rohan2003bernard <rohan2003boss@gmail.com>
2025-02-09 18:09:58 +0530

    changes made in password encryption

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,34 +2,42 @@ import React from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 import { useRef } from 'react'
+import CryptoJS from 'crypto-js'
 
 const Login = () => {
-    const data=useRef()
-    const password=useRef()
-    const onChange = () => {
-        localStorage.setItem('username',data.current.value)
-        localStorage.setItem('password',password.current.value)
-    }
+    const data = useRef()
+    const password = useRef()
     
+    const onChange = (event) => {
+        event.preventDefault()
+        const encryptedPassword = CryptoJS.AES.encrypt(password.current.value, 'secret-key').toString()
+        localStorage.setItem('username', data.current.value)
+        localStorage.setItem('password', encryptedPassword)
+    }
+
+    const getPassword = () => {
+        const encryptedPassword = localStorage.getItem('password')
+        if (encryptedPassword) {
+            const bytes = CryptoJS.AES.decrypt(encryptedPassword, 'secret-key')
+            // return bytes.toString(CryptoJS.enc.Utf8)
+            console.log(bytes.toString(CryptoJS.enc.Utf8))
+        }
+        return ''
+    }
 
     return (
-
-        <form className='login' onSubmit={()=>onChange}>
+        <form className='login' onSubmit={onChange}>
             <div className="title">
                 <div className="login2">Login</div>
             </div>
             <div className="credentials">
-                <input className='username' placeholder='Username' type="username" ref={data} />
-                <input className='password' placeholder='Password' type="password" ref={password}/>
+                <input className='username' placeholder='Username' type="text" ref={data} />
+                <input className='password' placeholder='Password' type="password" ref={password} />
             </div>
             <a href='#' className="msg">Forgot Password</a>
             <Link to='/signup' className='signup msg'>New User</Link>
             <button type="submit" className='submit'>Submit</button>
         </form>
-
-
-
-
     )
 }
 
--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -2,31 +2,45 @@
 import { useRef } from 'react'
 import './Signup.css'
 import { Link } from 'react-router-dom'
+import CryptoJS from 'crypto-js'
 
 const Signup = () => {
-    const name=useRef()
-    const organization=useRef()
-    const email=useRef()
-    const password=useRef()
-    const re_password=useRef()
-    const onStore=()=>{
-        localStorage.setItem('Name',name.current.value)
-        localStorage.setItem('organization',organization.current.value)
-        localStorage.setItem('email',email.current.value)
-        localStorage.setItem('password',password.current.value)
-        localStorage.setItem('re_password',re_password.current.value)
+    const name = useRef()
+    const organization = useRef()
+    const email = useRef()
+    const password = useRef()
+    const re_password = useRef()
+    const onStore = (event) => {
+        event.preventDefault()
+        const encryptedPassword = CryptoJS.AES.encrypt(password.current.value, 'secret-key').toString()
+        localStorage.setItem('Name', name.current.value)
+        localStorage.setItem('organization', organization.current.value)
+        localStorage.setItem('email', email.current.value)
+        localStorage.setItem('password', encryptedPassword)
+        localStorage.setItem('re_password', re_password.current.value)
     }
+
+    const getPassword = () => {
+        const encryptedPassword = localStorage.getItem('password')
+        if (encryptedPassword) {
+            const bytes = CryptoJS.AES.decrypt(encryptedPassword, 'secret-key')
+            // return bytes.toString(CryptoJS.enc.Utf8)
+            console.log(bytes.toString(CryptoJS.enc.Utf8))
+        }
+        return ''
+    }
+
     return (
 
-        <form className='Signup' onSubmit={()=>onStore}>
+        <form className='Signup' onSubmit={onStore}>
             <div className="title">
                 <div className="Signup2">Signup</div>
             </div>
             <div className="credentials">
                 <input className='name' placeholder='Name' type="name" ref={name} />
-                <input className='organization' placeholder='Organization' type="text" ref={organization}/>
+                <input className='organization' placeholder='Organization' type="text" ref={organization} />
                 <input className='email' placeholder='Email' type="email" ref={email} />
-                <input className='password' placeholder='Password' type="password" ref={password}/>
+                <input className='password' placeholder='Password' type="password" ref={password} />
                 <input className='re-password password' placeholder='Retype Password' type="password" ref={re_password} />
             </div>
             <Link to='/login' className='signup msg'>Existing User</Link>
